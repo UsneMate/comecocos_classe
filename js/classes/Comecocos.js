efc90faec081aa0818ff5c9dcd061e2431979e3d
UsneMate <mmanzano@inscaparrella.cat>
2025-02-09 21:47:05 +0100

    Merge remote-tracking branch 'origin/master'

---
+++ js/classes/Comecocos.js
@@ -0,0 +1,73 @@
+// Comecocos.js
+
+export class Comecocos {
+  constructor(x, y, radi, color) {
+    this.xCoord = x;
+    this.yCoord = y;
+    this.radi = radi;
+    this.color = color;
+    this.angleInici = 40;
+    this.angleFi = 330;
+  }
+
+  // Dibuixar el comecocos
+  draw() {
+    fill(this.color);
+    arc(this.xCoord, this.yCoord, this.radi, this.radi, this.angleInici, this.angleFi);
+  }
+
+  // Moure el comecocos amunt
+  moveUp() {
+    this.yCoord -= 15;
+    this.angleInici = 300;
+    this.angleFi = 240;
+  }
+
+  // Moure el comecocos avall
+  moveDown() {
+    this.yCoord += 15;
+    this.angleInici = 125;
+    this.angleFi = 55;
+  }
+
+  // Moure el comecocos a l'esquerra
+  moveLeft() {
+    this.xCoord -= 15;
+    this.angleInici = 210;
+    this.angleFi = 135;
+  }
+
+  // Moure el comecocos a la dreta
+  moveRight() {
+    this.xCoord += 15;
+    this.angleInici = 40;
+    this.angleFi = 330;
+  }
+
+  // Assegurar que el comecocos no surti del canvas
+  preventOutOfBounds() {
+    // Assegurem que no surti per dalt o per baix
+    if (this.yCoord < 20 + this.radi / 2) {
+      this.yCoord = 20 + this.radi / 2;
+    }
+    if (this.yCoord > 500 - 20 - this.radi / 2) {
+      this.yCoord = 500 - 20 - this.radi / 2;
+    }
+
+    // Assegurem que no surti per l'esquerra ni per la dreta
+    if (this.xCoord < 20 + this.radi / 2 && (this.yCoord <= 210 || this.yCoord >= 270)) {
+      this.xCoord = 20 + this.radi / 2;
+    }
+    if (this.xCoord > 600 - 20 - this.radi / 2 && (this.yCoord <= 200 || this.yCoord >= 270)) {
+      this.xCoord = 600 - 20 - this.radi / 2;
+    }
+
+    // Si el comecocos surt del canvas, ha de reapareixer a l'altre costat
+    if (this.xCoord > 600 && (this.yCoord > 200 && this.yCoord < 270)) {
+      this.xCoord = 0;
+    }
+    if (this.xCoord < 0 && (this.yCoord > 200 && this.yCoord < 270)) {
+      this.xCoord = 600;
+    }
+  }
+}
--- js/sketch.js
+++ js/sketch.js
@@ -1,36 +1,47 @@
-// variables globals
+import { Comecocos } from "./classes/Comecocos.js"; // Importar la classe Comecocos
 
-// coordenades de la mida del canvas
+// Coordenades de la mida del canvas
 const xCanvas = 600;
 const yCanvas = 500;
 
-// coordenades comecocos
-let xCoordComecocos = 100;
-let yCoordComecocos = 240;
-const radiComecocos = 40;
-let angleIniciComecocos = 40;
-let angleFiComecocos = 330;
-const colorComecocos = "Yellow";
-
-// variable amb la velocitat de moviment
-const velocitat = 15;
+// Creem una instància del comecocos
+let comecocos;
 
 /**
- *
+ * Configuració inicial
  */
 function setup() {
   createCanvas(xCanvas, yCanvas);
   angleMode(DEGREES);
+  comecocos = new Comecocos(100, 240, 40, "Yellow"); // Creem el comecocos
 }
 
 /**
- *
+ * Funció de dibuix continu
  */
 function draw() {
   background(220);
   drawParedsVerticals();
   drawParedsHoritzontals();
-  drawComecocos();
+  comecocos.draw(); // Dibuixem el comecocos
+
+  // Comprovem si el comecocos surt dels límits i corregim la seva posició
+  comecocos.preventOutOfBounds();
+}
+
+/**
+ * Funció per moure el comecocos amunt
+ */
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    comecocos.moveUp();
+  } else if (keyCode === DOWN_ARROW) {
+    comecocos.moveDown();
+  } else if (keyCode === LEFT_ARROW) {
+    comecocos.moveLeft();
+  } else if (keyCode === RIGHT_ARROW) {
+    comecocos.moveRight();
+  }
 }
 
 // funció per dibuixar les pareds verticals
@@ -55,73 +66,6 @@ function drawParedsHoritzontals() {
   rect(20, 480, 559, 20);
 }
 
-// dibuixo el comecocos
-/**
- *
- */
-function drawComecocos() {
-  fill(colorComecocos);
-  arc(xCoordComecocos, yCoordComecocos, radiComecocos, radiComecocos, angleIniciComecocos, angleFiComecocos);
-}
-
-// Funció per detectar tecles i moure el Comecocos
-/**
- *
- */
-function keyPressed() {
-  if (keyCode === UP_ARROW) {
-    yCoordComecocos -= velocitat;
-    angleIniciComecocos = 300;
-    angleFiComecocos = 240;
-  } else if (keyCode === DOWN_ARROW) {
-    yCoordComecocos += velocitat;
-    angleIniciComecocos = 125;
-    angleFiComecocos = 55;
-  } else if (keyCode === LEFT_ARROW) {
-    xCoordComecocos -= velocitat;
-    angleIniciComecocos = 210;
-    angleFiComecocos = 135;
-  } else if (keyCode === RIGHT_ARROW) {
-    xCoordComecocos += velocitat;
-    angleIniciComecocos = 40;
-    angleFiComecocos = 330;
-  }
-
-  // Assegurar que el Comecocos no surti del canvas.
-  // 20 és el gruix de la pared de la superior
-  // radiComecocos/2, perquè les coordenades es conten des del centre
-  if (yCoordComecocos < 20 + radiComecocos / 2) {
-    yCoordComecocos = 20 + radiComecocos / 2;
-  }
-  // if que detecta les pareds de la part inferior, se li ha de restar a       l'alçada
-  // del canvas de y l'amplada de la pared - el radi del comecocos
-  if (yCoordComecocos > yCanvas - 20 - radiComecocos / 2) {
-    yCoordComecocos = yCanvas - 20 - radiComecocos / 2;
-  }
-
-  // Bloquejar el moviment lateral excloent les obertures laterals
-  // L'alçada del pas és de 100
-  // primer limitem la sortida de l'esquerra
-  if (xCoordComecocos < 20 + radiComecocos / 2 && (yCoordComecocos <= 210 || yCoordComecocos >= 300)) {
-    xCoordComecocos = 20 + radiComecocos / 2;
-  }
-  // Ara limitem la sortida de la dreta
-  if (xCoordComecocos > xCanvas - 20 - radiComecocos / 2 && (yCoordComecocos <= 200 || yCoordComecocos >= 300)) {
-    xCoordComecocos = xCanvas - 20 - radiComecocos / 2;
-  }
-
-  // Només deixem passar si passa entre 200 i 300 en vertical
-  // si el comecocos sobrepassa el canvas, comença un altre cop a coordenada de x 0
-  if (xCoordComecocos > xCanvas && (yCoordComecocos > 200 && yCoordComecocos < 300)) {
-    xCoordComecocos = 0;
-  }
-  // si el comencocos sobrepassa la pared de l'esquerra, comença un altre cop
-  // a la dreta, que és l'amplada del canvas
-  if (xCoordComecocos < 0 && (yCoordComecocos > 200 && yCoordComecocos < 300)) {
-    xCoordComecocos = xCanvas;
-  }
-}
 globalThis.setup = setup;
 globalThis.draw = draw;
-// globalThis.preload = preload;
 globalThis.keyPressed = keyPressed;
