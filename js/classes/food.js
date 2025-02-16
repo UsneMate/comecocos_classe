43593df5fd7686ff5f17504806f960e03c082ce5
UsneMate <mmanzano@inscaparrella.cat>
2025-02-16 14:09:03 +0100

    Merge remote-tracking branch 'origin/master'

--- js/classes/food.js
+++ js/classes/food.js
@@ -1,64 +1,14 @@
-/** @class Circle representing a circle. */
-class Circle {
-  /**
-   * Creates an instance of Circle.
-   *
-   * @author eduardo
-   * @param {number} r The desired radius of the circle.
-   */
-  constructor(r) {
-    /** @private */ this.radius = r
-    /** @private */ this.circumference = 2 * Math.PI * r
-  }
 
-  /**
-   * Creates a new Circle from a diameter.
-   *
-   * @param {number} d The desired diameter of the circle.
-   * @return {Circle} The new Circle object.
-   */
-  static fromDiameter(d) {
-    return new Circle(d / 2)
-  }
+import { gameObject } from './gameObject.js';
 
-  /**
-   * Calculates the circumference of the Circle.
-   *
-   * @deprecated since 1.1.0; use getCircumference instead
-   * @return {number} The circumference of the circle.
-   */
-  calculateCircumference() {
-    return 2 * Math.PI * this.radius
+export class Food extends gameObject {
+  constructor(row, column) {
+    super(row, column);
+    this.pointsFood = 10;
   }
-
-  /**
-   * Returns the pre-computed circumference of the Circle.
-   *
-   * @return {number} The circumference of the circle.
-   * @since 1.1.0
-   */
-  getCircumference() {
-    return this.circumference
-  }
-
-  /**
-   * Find a String representation of the Circle.
-   *
-   * @override
-   * @return {string} Human-readable representation of this Circle.
-   */
   toString() {
-    return `[A Circle object with radius of ${this.radius}.]`
+    console.log( `Food at row ${this.rowNumber}
+    and column ${this.columnObjectNumber}
+    with ${this.pointsFood} points`);
   }
-}
-
-/**
- * Prints a circle.
- *
- * @param {Circle} circle
- */
-function printCircle(circle) {
-  /** @this {Circle} */
-  function bound() { console.log(this) }
-  bound.apply(circle)
 }
--- js/sketch.js
+++ js/sketch.js
@@ -1,162 +1,221 @@
-import { Comecocos } from "./classes/Comecocos.js"; // Importar la classe Comecocos
-import { Food } from "./classes/food.js"; // Importar la classe Food
-
-const xCanvas = 600;
-const yCanvas = 600;
-
-const gameBoard = [
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  [1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 1],
-  [1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 1],
-  [1, 2, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1],
-  [1, 2, 0, 1, 0, 0, 0, 0, 0, 2, 0, 1, 2, 2, 1],
-  [1, 2, 2, 0, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2, 1],
-  [0, 0, 2, 0, 0, 0, 0, 1, 0, 2, 2, 0, 2, 2, 0],
-  [0, 0, 2, 0, 0, 0, 1, 1, 1, 2, 2, 0, 2, 2, 0],
-  [1, 0, 2, 0, 0, 0, 1, 1, 1, 2, 2, 0, 2, 2, 1],
-  [1, 0, 2, 1, 0, 0, 0, 0, 0, 2, 2, 1, 2, 2, 1],
-  [1, 0, 2, 1, 0, 0, 0, 0, 0, 2, 2, 1, 2, 2, 1],
-  [1, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 0, 1],
-  [1, 2, 0, 0, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 1],
-  [1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+import { gameObject } from "./classes/gameObject.js";
+import { Pacman } from "./classes/pacman.js";
+import { Food } from "./classes/food.js";
+import {configGame} from "./constants.js";
+import {ErrorPac} from "./classes/errorPac.js";
+//0 -> pallcman, 1 -> roca, 2 -> food
+/*
+const map = [
+  [0, 1, 1, 1],
+  [2, 1, 1, 2],
+  [2, 1, 1, 2],
+  [1, 1, 1, 1]
 ];
 
-let comecocos;
-let imgMur;
+const map = [
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 2, 2, 2, 1, 2, 2, 2, 2, 1],
+  [1, 2, 1, 2, 1, 2, 1, 2, 2, 1],
+  [1, 2, 1, 3, 2, 2, 1, 2, 2, 1],
+  [1, 2, 2, 2, 1, 2, 2, 3, 2, 1],
+  [1, 2, 1, 2, 1, 2, 1, 2, 2, 1],
+  [1, 2, 1, 2, 2, 3, 1, 2, 2, 1],
+  [1, 2, 1, 1, 1, 1, 1, 3, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 4, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+];
+const ROWS = 10;
+const COLUMNS = 10;
+export const IMAGE_SIZE = 32;
+export const WIDTH_CANVAS = IMAGE_SIZE * COLUMNS;
+const HEIGHT_CANVAS = IMAGE_SIZE * ROWS;
+
+const EXTRA_SIZE_HEIGHT=200;
+ */
+let imgRock;
+let numberImagesLoaded = 0;
+const arrRocks = [];
+
 let imgFood;
-const foodItems = []; // Array per emmagatzemar els objectes de menjar
-let score = 0; // Punts inicials
+const arrFood = [];
 
-/**
- * Càrrega de la imatge de les parets
- */
+let imgPacmanLeft;
+let imgPacmanRight, imgPacmanUp, imgPacmanDown, imgPacman;
+let myPacman;
+let wakaSound;
+let timer = 0;
+let startTimeGame = 0;
+let endTimeGame = 0;
+
 function preload() {
-  imgMur = loadImage("../img/roca.png"); // Carregar la imatge de la paret
-  imgFood = loadImage("../img/food.png"); // Carregar la imatge del menjar
+  imgRock = loadImage("../media/roca.png", handleImage, handleError);
+  imgFood = loadImage("../media/food.png", handleImage, handleError);
+  imgPacman = loadImage("../media/pacLeft.png", handleImage, handleError);
+  imgPacmanRight = loadImage("../media/pacRight.png", handleImage, handleError);
+  imgPacmanUp = loadImage("../media/pacUp.png", handleImage, handleError);
+  imgPacmanLeft = loadImage("../media/pacLeft.png", handleImage, handleError);
+  imgPacmanDown = loadImage("../media/pacDown.png", handleImage, handleError);
+  wakaSound = loadSound("../media/audio/WakaWaka.mp3");
 }
 
-/**
- * Configuració inicial
- */
+function handleError() {
+  console.error("Error carregar alguna imatge");
+  try {
+    throw new ErrorPac(20, "Falta imatge per carregar");
+  } catch (error) {
+    console.error("Error carregar alguna imatge");
+    showError();
+  }
+}
+
+function handleImage() {
+  console.error("Images carregada correctament");
+  numberImagesLoaded++;
+}
+
 function setup() {
-  createCanvas(xCanvas, yCanvas);
-  angleMode(DEGREES);
-
-  comecocos = new Comecocos(100, 240, 40, "Yellow"); // Creem el comecocos
-
-  // Crear els objectes de menjar per a les cel·les amb valor 0
-  for (let row = 0; row < gameBoard.length; row++) {
-    for (let col = 0; col < gameBoard[row].length; col++) {
-      if (gameBoard[row][col] === 2) {
-        foodItems.push(new Food(col, row, 10)); // Afegir un objecte de menjar amb 10 punts
+  createCanvas(configGame.WIDTH_CANVAS, configGame.HEIGHT_CANVAS + configGame.EXTRA_SIZE_HEIGHT).parent("sketch-pacman");
+  for (let filaActual = 0; filaActual < configGame.ROWS; filaActual++) {
+    for (let columnaActual = 0; columnaActual < configGame.COLUMNS; columnaActual++) {
+      if (configGame.map[filaActual][columnaActual] === 1) {
+        const roca = new gameObject(filaActual, columnaActual);
+        // console.log("\n he creat una roca a posicio: fila -> " + filaActual + " columna -> " + columnaActual);
+        arrRocks.push(roca);
       }
-    }
-  }
-}
+      else if (configGame.map[filaActual][columnaActual] === 2) {
+        const food = new Food(filaActual, columnaActual);
+        //  console.log("\n he craat una food a posicio: fila -> " + filaActual + " columna -> " + columnaActual);
+        arrFood.push(food);
+      }
+      else if (configGame.map[filaActual][columnaActual] === 3) {
+        myPacman = new Pacman(filaActual, columnaActual);
+        // console.log("\n he craat una pacaman a posicio: fila -> " + filaActual + " columna -> " + columnaActual);
+      }
+      else {
+        //Error objecte no defini
 
-/**
- * Funció de dibuix continu
- */
+      }
+    } // fi for columnes
+  } // fi for files
+  console.log("array rocks mida es : ", arrRocks.length);
+  console.log("array foods mida es : ", arrFood.length);
+  startTimeGame = millis();
+} // fi setup
+
 function draw() {
-  background(220);
-
-  const midaCasella = 40;
-
-  for (let row = 0; row < gameBoard.length; row++) {
-    for (let col = 0; col < gameBoard[row].length; col++) {
-      const x = col * midaCasella; // Posició X de la cel·la
-      const y = row * midaCasella; // Posició Y de la cel·la
-
-      // Dibuixar segons el valor de gameBoard[row][col]
-      if (gameBoard[row][col] === 1) {
-        image(imgMur, x, y, midaCasella, midaCasella); // Dibuixar roca
-      }
-    }
+  background(171, 248, 168);
+  // arrRocks.forEach((roca) => roca.showObject(imgRock));
+  //Pintem roques
+  for (let i = 0; i < arrRocks.length; i++) {
+    arrRocks[i].showObject(imgRock);
   }
 
-  // Dibuixar els objectes de menjar
-  for (let i = foodItems.length - 1; i >= 0; i--) {
-    const food = foodItems[i];
-    food.draw(imgFood);
-
-    // Comprovar si el comecocos ha recollit el menjar
-    if (comecocos.xCoord < food.x * 40 + 30 &&
-      comecocos.xCoord + 40 > food.x * 40 &&
-      comecocos.yCoord < food.y * 40 + 30 &&
-      comecocos.yCoord + 40 > food.y * 40) {
-      // Eliminar el menjar de la llista
-      foodItems.splice(i, 1);
-      // Afegir punts
-      score += food.points;
-    }
+  //Pintem food
+  for (let i = 0; i < arrFood.length; i++) {
+    arrFood[i].showObject(imgFood);
+  }
+  //comprovar colisions pacman amb roques
+  for (let i = 0; i < arrRocks.length; i++) {
+    myPacman.testCollideRock ( arrRocks[i]);
   }
 
-  comecocos.draw(); // Dibuixar el comecocos un cop fora del bucle
-  comecocos.preventOutOfBounds(); // Comprovar si el comecocos surt del canvas
-}
+//comprovar colisions pacman amb food
+  for (let i = 0; i < arrFood.length; i++) {
+    let resultTest = myPacman.testCollideFood(arrFood[i]);
+    if (resultTest) {
+      myPacman.scorePacman = myPacman.scorePacman + arrFood[i].pointsFood;
+      arrFood.splice(i, 1);
+    }
+  }
+  //pINTEM ScoreBoard
+  // textFont(font);
+  textSize(20);
+  textAlign(CENTER, CENTER);
+  timer = parseInt( millis() - startTimeGame);
+  text("Score: " + myPacman.scorePacman, 150, configGame.HEIGHT_CANVAS + 50);
+//   text("Score: " + myPacman.scorePacman, 150, HEIGHT_CANVAS + 50);
+  text("Time: " + timer, 150, configGame.HEIGHT_CANVAS + 100);
+  //Pintem pacman
+  //myPacman.showObject(imgPacman);
+  switch(myPacman.directionPacman){
+    case 1: //Move right
+      myPacman.showObject(imgPacmanRight);
+      break;
+    case 2: //Move up
+      myPacman.showObject(imgPacmanUp);
+      break;
+    case 3: //Move left
+      myPacman.showObject(imgPacmanLeft);
+      break
+    case 4: //Move down
+      myPacman.showObject(imgPacmanDown);
+      break;
+    default : myPacman.showObject(imgPacman);
 
-/**
- *
- */
+  }
+  testFinishGame();
+  wakaSound.play();
+} // fi draw
+
 function keyPressed() {
-  const midaCasella = 40;
-
-  // Moure el comecocos segons la tecla premuda
-  if (keyCode === UP_ARROW) {
-    comecocos.moveUp();
-    if (isWall(comecocos.xCoord, comecocos.yCoord)) {
-      comecocos.yCoord += 30; // Desfer el moviment si hi ha una roca
-    }
-  } else if (keyCode === DOWN_ARROW) {
-    comecocos.moveDown();
-    if (isWall(comecocos.xCoord, comecocos.yCoord)) {
-      comecocos.yCoord -= 30; // Desfer el moviment si hi ha una roca
-    }
+  if (keyCode === RIGHT_ARROW) {
+    console.log("Dreta");
+    myPacman.moveRight();
   } else if (keyCode === LEFT_ARROW) {
-    comecocos.moveLeft();
-    if (isWall(comecocos.xCoord, comecocos.yCoord)) {
-      comecocos.xCoord += 30; // Desfer el moviment si hi ha una roca
-    }
-  } else if (keyCode === RIGHT_ARROW) {
-    comecocos.moveRight();
-    if (isWall(comecocos.xCoord, comecocos.yCoord)) {
-      comecocos.xCoord -= 30; // Desfer el moviment si hi ha una roca
-    }
+    console.log("Esquerra");
+    myPacman.moveLeft();
+  } else if (keyCode === UP_ARROW) {
+    myPacman.moveUp();
+  } else if (keyCode === DOWN_ARROW) {
+    myPacman.moveDown();
+  } else {
+    console.log("Error, tecla no reconeguda");
+    let error = new ErrorPac(101, "Press a valid key");
+    error.toString();
   }
 }
 
-/**
- * funció per verificar si tenim una pared
- * @param x
- * @param y
- * @returns {boolean}
- */
-function isWall(x, y) {
-  // Calculem la posició de la casella dins de gameBoard
-  const col = Math.floor(x / 40); // Calcular la columna
-  const row = Math.floor(y / 40); // Calcular la fila
+function showError(){
+  let errorImage = new ErrorPac(105, "Error 2loading image");
+  errorImage.toString();
+  const parent = document.getElementById("error-holder");
+  const node = document.createElement("media");
+  node.setAttribute("src", "./media/tristesa.webp");
+  node.setAttribute("alt", "Imatge Error");
+  node.setAttribute("width", 300);
+  node.setAttribute("height", 300);
 
-  // Comprovem si la casella és una roca (valor 1)
-  return gameBoard[row][col] === 1;
+  parent.appendChild(node);
+  noLoop();
+  remove();
 }
 
-/**
- * Funció per verificar si hi ha menjar en una posició donada
- * @param x Coordenada X en píxels
- * @param y Coordenada Y en píxels
- * @returns {boolean} Retorna cert si hi ha menjar en la posició
- */
-function isFood(x, y) {
-  // Calculem la posició de la casella dins de gameBoard
-  const col = Math.floor(x / 40); // Calcular la columna
-  const row = Math.floor(y / 40); // Calcular la fila
-
-  // Comprovem si la casella conté menjar (valor 2)
-  return gameBoard[row][col] === 2;
+function testFinishGame(){
+  if (arrFood.length === 0){
+    //Fi del joc
+    noLoop();
+    let theconfirm =confirm("Fi del joc, has guanyat. Desitja jugar una altra partida ?");
+    loop`();`
+    if( theconfirm)
+    {
+      restartGame();
+    }
+    else {
+      alert("Gracies per jugar");
+    }
+    loop();
+  }/*
+  else if {
+    //test if loose game
+  }
+  else {
+    //continume
+  }*/
 }
-
+/*globalThis: globalThis object. This is done to ensure
+that the p5.js library can call these functions when needed.
+ */
 globalThis.setup = setup;
 globalThis.draw = draw;
-globalThis.keyPressed = keyPressed;
 globalThis.preload = preload;
+globalThis.keyPressed = keyPressed;
